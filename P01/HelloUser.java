a6c9a0017bff18da6c8f833ce84d08ce86924bef
espon360 <atd3232@mavs.uta.edu>
2023-08-27 22:54:34 -0500

    message

---
+++ P01/HelloUser.java
@@ -0,0 +1,9 @@
+public class HelloUser
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("Hello user: ");
+		System.getProperty("user.name");
+	}
+}
---
+++ P01/full_credit/Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		String name = "An Duong";
+		System.out.println("Hello, " + name);
+	}
+}
