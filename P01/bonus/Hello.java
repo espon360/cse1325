9f341d48dce3a5d66c9e77b0c7a78cc9a2aca3bc
espon360 <atd3232@mavs.uta.edu>
2023-08-27 23:27:21 -0500

    message

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class HelloScan
+public class Hello
 {
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
 		System.out.println("Please enter your name: ");
 		String name = in.nextLine();
-		System.out.println("Your name is: " + name);
+		System.out.println("Hello, " + name);
 	}
 }
---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,8 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		String user = System.getProperty("user.name");
+		System.out.println("Hello, " + user);
+	}
+}
