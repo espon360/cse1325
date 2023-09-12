178a43c4c2f71cbe51bf6d0448e93ed48c182e91
espon360 <atd3232@mavs.uta.edu>
2023-09-12 04:01:14 -0500

    message

---
+++ P03/full_credit/LibraryManager.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class LibraryManager
+{
+        public static void main(String[] args)
+        {
+                Scanner in = new Scanner(System.in);
+                Library l = new Library();
+                
+        }
+}
---
+++ P03/full_credit/Publication.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.time.LocalDate;
+
+public class Publication
+{
+        public String title;
+        public String author;
+        public int copyright; // the year it is copyrighted in
+        private String loanedTo; // patron who checked book
+        private LocalDate dueDate; // when is book due (14 days after today)
+
+        public Publication(String title, String author, int copyright)
+        {
+                this.title = title;
+                this.author = author;
+                LocalDate d = LocalDate.now();
+                int year = d.getYear();
+                if(copyright < 1900 || copyright > year)
+                        throw new IllegalArgumentException("Copyright year older than 1900 or set in the future");
+                else this.copyright = copyright;
+        }
+
+        public void checkOut(String patron)
+        {
+                loanedTo = patron;
+                LocalDate d = LocalDate.now();
+                dueDate = d.plusWeeks(2);
+        }
+
+        public String toString()
+        {
+                return loanedTo + " checked out \'" + title + "\' by "
+                + author + "\n Copyrighted: " + copyright + "\n Please return by: "
+                + dueDate;
+        }
+}
