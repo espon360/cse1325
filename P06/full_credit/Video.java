019cd9d0a2f399d9cc3afa7452f4d4ef5882fdc4
espon360 <atd3232@mavs.uta.edu>
2023-10-08 00:14:02 -0500

    made video

---
+++ P06/full_credit/Video.java
@@ -0,0 +1,23 @@
+package library;
+
+import java.time.Duration;
+
+public class Video extends Publication
+{
+        public class InvalidRuntimeException extends ArithmeticException
+        {
+                public InvalidRuntimeException(){super();}
+                public InvalidRuntimeException(String message){super(message);}
+                public InvalidRuntimeException(String title, int runtime) {super(title + " has invalid runtime: " + runtime);}
+        }
+
+        public Video(String title, String author, int copyright, int runtime)
+        {
+                super(title, author, copyright);
+                if(runtime < 1)
+                    throw new InvalidRuntimeException(title, runtime);
+                this.runtime = Duration.ofMinutes(runtime);
+        }
+
+        private Duration runtime;
+}
